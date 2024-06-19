62c640e8bb617af8dab70ee771ad396f69db5fcb
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-06-19 22:10:43 +0330

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -180,6 +180,7 @@ check_root
 while true; do
     sleep 1.5
     echo "\033[0;36mSelect an option:"
+    echo "${RED}Error:${RESET} Something went wrong."
     echo "1. Install HAProxy"
     echo "2. Add IPs and Ports to Forward"
     echo "3. Clear Configurations"
